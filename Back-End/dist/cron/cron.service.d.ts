fba5691b7bbc3ae251da3434270b5c152273537c
AsadJaved2 <asadjaved744@gmail.com>
2024-09-20 21:14:44 +0500

    refactor

--- Back-End/dist/cron/cron.service.d.ts
+++
@@ -1,7 +0,0 @@
-import { ChatGateway } from '../chat/chat-gateway';
-export declare class MessageArchiveService {
-    private readonly chatGateway;
-    constructor(chatGateway: ChatGateway);
-    handleCron(): void;
-    private archiveMessages;
-}
